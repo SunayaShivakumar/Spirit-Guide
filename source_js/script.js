52e8fe0c771eeca5022f1926dc3c0a123dda1180
SunayaShivakumar <sshivak2@illinois.edu>
2016-09-20 01:30:21 +0000

    mp1 submission

--- source_js/script.js
+++ source_js/script.js
@@ -1,16 +1,102 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
+//smooth scrolling
+$(document).ready(function(){
+	$(function() {
+		$("nav a").click(function() {
+			var go = $(this.hash).offset().top;
+			$('html,body').animate({ scrollTop: go}, 2000);
+		});
+	}); 
+});
+
+//resizing navbar
+$(document).on("scroll", function() {
+	if($(document).scrollTop()>300) {
+		$("header").removeClass("large").addClass("small");
+	} else {
+		$("header").removeClass("small").addClass("large");
+	}
+	scrollfunc();
+});
+
+//carousel
+var counter = 0,
+    $items = $('#boo .show, #boo .hide'),
+    numItems = $items.length;
+
+var current = function(){
+    var itemToShow = Math.abs(counter%numItems);
+    var itemToHide = Math.abs((counter+1)%numItems);
+    var itemToHide2 = Math.abs((counter+2)%numItems);
+    $items.removeClass('show'); 
+    $items.removeClass('hide');
+    $items.eq(itemToHide).addClass('hide');
+    $items.eq(itemToHide2).addClass('hide');   
+    $items.eq(itemToShow).addClass('show'); 
+};
+
+$('.next').on('click', function(){
+    counter++;
+    current(); 
+});
+$('.prev').on('click', function(){
+    counter--;
+    current(); 
+});
+
+//modal
+var modal = document.getElementById('modd');
+var img = document.getElementById('button');
+var modalImg = document.getElementById('full');
+var captionText = document.getElementById('caption')[0];
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = document.getElementById('full').src;
+    captionText.innerHTML = captionText;
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+  modal.style.display = "none";
 }
 
-function highlightThis(event) {
-    //event.stopPropagation();
+//video
+var vid = document.getElementById("smokyvid");
+vid.playbackRate = 0.5;
 
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+//position indicator
+var lastId,
+    topMenu = $("ul"),
+    topMenuHeight = topMenu.outerHeight()+10,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 500);
+  e.preventDefault();
+});
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
